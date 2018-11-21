5375ef80ed307aa34954c4e5adc6c95a17f27c03
joseph-allen <joseph.allen.1994@hotmail.com>
2018-11-21 23:13:59 +0000

    Send a Tweet out with the content from tweets to a user

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from auth import consumer_key,consumer_secret,access_token, access_token_secret
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import tweepy
 import json
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+
     def on_data(self, data):
         obj = json.loads(data)
-        # Write Sentiment
-        print(obj['text'])
+        text = obj['text']
+        # TODO: Calculate Sentiment
         # Tweet out response
+        api = tweepy.API(auth)
+        # api.update_status(text)
+        print("Tweeting... " + text)
+
         return True
 
     def on_error(self, status):
@@ -30,4 +36,5 @@ if __name__ == '__main__':
     auth = setup_auth()
 
     stream = Stream(auth, l)
-    stream.filter(track=['basketball'])
+    # filter for tweets at a particular user only, in this case realdonaldtrump
+    stream.filter(track=['@realdonaldTrump'])
